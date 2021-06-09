1cb60807eaea83977a6690aefe4f8c21a4a47a5e
statueofdavid <dmpersonalemail@gmail.com>
2021-06-09 00:37:22 -0400

    learning that slack bot life with the bolt framework

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
+const { App } = require('@slack/bolt');
+const store = require('./store');
+
 const app = new App({
-	token: process.env.SLACK_BOT_TOKEN,
-	signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN
 });
 
+app.event('app_home_opened', async ({ event, say }) => {
+  // Look up the user from DB
+  let user = store.getUser(event.user);
+
+  if (!user) {
+    user = {
+      user: event.user,
+      channel: event.channel
+    };
+    store.addUser(user);
+
+    await say(`Hello world, and welcome <@${event.user}>!`);
+    console.log(event);
+  } else {
+    await say('Hi again!');
+    console.log(event);
+  }
+});
+
+// Listens for messages containing "knock knock" and responds with an italicized "who's there?"
+app.message('knock knock', async ({ event, say }) => {
+  await say(`_Who's there?_`);
+  app.message('boo', async ({event, say}) => {
+    await say(`_boo who?_`);
+      app.message('do not cry', async ({event, say}) => {
+      await say(`_:)_`);
+      });
+  });
+});
+
+app.event('app_mention', async ({ event, say, payload }) => {
+  console.log(payload);
+  await say(`_what it do, <@${event.user}>_`);
+
+});
+
+// Start your app
 (async () => {
-	await app.start(process.env.PORT || 3000);
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
 })();
-
-app.event('app_home_opened', ({ event, say }) => {
-	say(`Hello world, <@${event.user}>!`);
-});
-
